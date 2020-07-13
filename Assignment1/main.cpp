ec67dc2311bb74c4561ba807dc1093cc0f90a999
SimarjitBilkhuSB <simarjitbilkhu@gmail.com>
2020-07-13 14:50:15 -0400

    Initial Commit

---
+++ Assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Simarjit Bilkhu on 7/13/20.
+//  Copyright © 2020 Simarjit Bilkhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
